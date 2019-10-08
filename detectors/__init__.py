7956c25c5844acb33c0533c3671ccdd370cb85a3
travisdonnell <travis.r.donnell@gmail.com>
2019-10-08 16:32:45 -0400

    bad rebase

--- detectors/__init__.py
+++ detectors/__init__.py
@@ -1,8 +1,9 @@
 from detectors.detectsecrets import DetectSecrets
 from detectors.gitsecrets import GitSecrets
-
+from detectors.trufflehog import TruffleHog
 # TODO: Turn this into a registry to match the notifiers pattern?
 AvailableDetectors = {
     'detect-secrets': DetectSecrets,
-    'git-secrets': GitSecrets
+    'git-secrets': GitSecrets,
+    'trufflehog': TruffleHog
 }
