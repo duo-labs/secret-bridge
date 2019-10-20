4499aa5ba1894604d37843a3a2c01a5283f6732d
travisdonnell <travis.r.donnell@gmail.com>
2019-10-20 13:04:25 -0400

    changed trufflehog to handle json output instead

--- detectors/trufflehog.py
+++ detectors/trufflehog.py
@@ -31,49 +31,24 @@ class TruffleHog(Detector):
         """
         self.logger.info("instantiating trufflehog")
         #dummy_repo_url value needed by trufflehog as filler field, since no remote repo is being used
-        sp = subprocess.run([self._binary_path, "--repo_path", ".", "dummy_repo_url"], cwd=repo_dir, capture_output=True)
+        sp = subprocess.run([self._binary_path, "--json", "--repo_path", ".", "dummy_repo_url"], cwd=repo_dir, capture_output=True)
 
         if sp.returncode not in [0, 1]:
             self.logger.error(sp.stderr.encode())
             raise Exception("Unknown error while running truffleHog.")
 
-        return self._output_to_findings(sp.stdout, commit_link)
-
-    def _output_to_findings(self, output, commit_link):
+        return self._output_json_findings(sp.stdout, commit_link)
+    
+    def _output_json_findings(self, output, commit_link):
+        findings = []
         if isinstance(output, bytes):
             output = output.decode()
 
-        Reason = ''
-        Hash = ''
-        Filepath = ''
-        Branch = ''
-        Commit = ''
-
-        findings = []
-
         for line in output.splitlines():
-            # trufflehog output is split across lines with
-            # *nix specific characters. cleaning cleaning
-            # and multi-line parsing needed to get clean fields
-            # across all result sets that are introduced as a
-            # single blob
+            json_line = json.loads(line)
 
-            parts = line.split(':')
-            if "Reason" in parts[0]:
-                Reason = parts[1].replace('\x1b[92m','').replace('\x1b[0m','')
-            if "Hash" in parts[0]:
-                Hash = parts[1].replace('\x1b[92m','').replace('\x1b[0m','')
-            if "Filepath" in parts[0]:
-                Filepath = parts[1].replace('\x1b[92m','').replace('\x1b[0m','')
-            if "Branch" in parts[0]:
-                Branch = parts[1].replace('\x1b[92m','').replace('\x1b[0m','')
-            if "Commit" in parts[0]:
-                Commit = parts[1].replace('\x1b[92m','').replace('\x1b[0m','')
-            if Commit is not '':
-                findings.append(Finding(Filepath, Reason, "unknown",None,commit_link+"/commit/"+Hash.strip()))
-                Reason = ''
-                Hash = ''
-                Filepath = ''
-                Branch = ''
-                Commit = ''
+            reason = json_line['reason']
+            results = json_line['stringsFound']
+            filename = json_line['path']
+            findings.append(Finding(filename, reason, str(results), link=commit_link))
         return findings
